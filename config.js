ea307c63da45906134d406b3b9410366d65f4894
Jesse Seeligsohn <jesseseeligsohn@gmail.com>
2022-06-06 18:36:59 -0400

    Modify config

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ const CONFIG = {
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
-	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
 	defaultLatitude: '37.775',
 	defaultLongitude: '-122.419',
 
@@ -158,7 +158,7 @@ const CONFIG = {
 		// },
 		{
 			icon: 'coffee',
-			id: '2',
+			id: '1',
 			links: [
 				{
 					name: 'Linkedin',
