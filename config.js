b0094768964c4b7cfb767089fe3af5275821cc9c
Hexamech <106445217+Hexamech@users.noreply.github.com>
2022-10-10 20:44:36 -0400

    Update config.js

--- config.js
+++ config.js
@@ -134,95 +134,47 @@ const CONFIG = {
 
 	// First Links Container
 	firstlistsContainer: [
-		// {
-		// 	icon: 'music',
-		// 	id: '1',
-		// 	links: [
-		// 		{
-		// 			name: 'Inspirational',
-		// 			link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-		// 		},
-		// 		{
-		// 			name: 'Classic',
-		// 			link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-		// 		},
-		// 		{
-		// 			name: 'Oldies',
-		// 			link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-		// 		},
-		// 		{
-		// 			name: 'Rock',
-		// 			link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-		// 		},
-		// 	],
-		// },
-		// {
-		// 	icon: 'coffee',
-		// 	id: '1',
-		// 	links: [
-		// 		{
-		// 			name: 'Linkedin',
-		// 			link: 'https://www.linkedin.com',
-		// 		},
-		// 		{
-		// 			name: 'Dribbble',
-		// 			link: 'https://www.dribbble.com',
-		// 		},
-		// 		{
-		// 			name: 'Trello',
-		// 			link: 'https://www.trello.com',
-		// 		},
-		// 		{
-		// 			name: 'Slack',
-		// 			link: 'https://www.slack.com',
-		// 		},
-		// 	],
-		// },
-	],
-
-	// Second Links Container
-	secondListsContainer: [
 		{
-			icon: 'binary',
+			icon: 'tv-retro',
 			id: '1',
 			links: [
 				{
-					name: 'Spotify',
-					link: 'https://www.spotify.com',
+					name: 'Inspirational',
+					link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
 				},
 				{
-					name: 'Reddit',
-					link: 'https://www.reddit.com',
+					name: 'Classic',
+					link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
 				},
 				{
-					name: 'Hashnode',
-					link: 'https://www.hashnode.com',
+					name: 'Oldies',
+					link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
 				},
 				{
-					name: 'Pocket',
-					link: 'https://www.pocket.com',
+					name: 'Rock',
+					link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
 				},
 			],
 		},
 		{
-			icon: 'github',
-			id: '2',
+			icon: 'coffee',
+			id: '1',
 			links: [
 				{
-					name: 'Front',
-					link: 'https://www.reddit.com/r/Frontend/',
+					name: 'Linkedin',
+					link: 'https://www.linkedin.com',
 				},
 				{
-					name: 'Rust',
-					link: 'https://www.reddit.com/r/rust/',
+					name: 'Dribbble',
+					link: 'https://www.dribbble.com',
 				},
 				{
-					name: 'Go',
-					link: 'https://www.reddit.com/r/golang/',
+					name: 'Trello',
+					link: 'https://www.trello.com',
 				},
 				{
-					name: 'Repos',
-					link: 'https://github.com/migueravila',
+					name: 'Slack',
+					link: 'https://www.slack.com',
 				},
 			],
 		},
